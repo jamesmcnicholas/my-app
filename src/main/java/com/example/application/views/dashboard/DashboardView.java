e29920e8879d7c017521f4e2e60da889410916eb
start.vaadin.com <no-reply@vaadin.com>
2024-07-08 13:07:27 +0000

    Generated project

---
+++ src/main/java/com/example/application/views/dashboard/DashboardView.java
@@ -0,0 +1,27 @@
+package com.example.application.views.dashboard;
+
+import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Menu;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+
+@PageTitle("Dashboard")
+@Menu(icon = "line-awesome/svg/pencil-ruler-solid.svg", order = 0)
+@Route(value = "")
+@RouteAlias(value = "")
+public class DashboardView extends Composite<VerticalLayout> {
+
+    public DashboardView() {
+        Button buttonPrimary = new Button();
+        getContent().setWidth("100%");
+        getContent().getStyle().set("flex-grow", "1");
+        buttonPrimary.setText("Button");
+        buttonPrimary.setWidth("min-content");
+        buttonPrimary.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        getContent().add(buttonPrimary);
+    }
+}
